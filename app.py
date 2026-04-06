30df34ae8a4279bbd3308eba00f1d4ab28b9356c
arthurpcidrao <arthurpcidrao@gmail.com>
2026-04-05 22:40:46 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Simulador de Índice Hash",
+    layout="wide"
+)
+
+
+st.title("Hello, Streamlit!")
+st.write("This is a simple Streamlit app.")
